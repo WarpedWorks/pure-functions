0a984b53ee5d4cace361056310eb73744e1badb0
Kathryn Parker <kparker@warpedworks.com>
2018-06-21 19:12:52 -0400

    class notes

--- public/exercise1/reducer.js
+++ public/exercise1/reducer.js
@@ -2,12 +2,15 @@ let initialState = {
 	counterValue: 0
 };
 
-// This reducer isn't pure! It updates the "counterValue" key of the original 
+// This reducer isn't pure! It updates the "counterValue" key of the original
 //     state object
 const reducer = (state = initialState, action) => {
 	if (action.type == "INCREMENT") {
-		state.counterValue++;
-		return state;
+		// state.counterValue++;
+		// return state;
+		return {
+			counterValue: state.counterValue + 1 //one more than the prvious value
+		}
 	} else {
 		return state;
 	}
--- public/exercise2/reducer.js
+++ public/exercise2/reducer.js
@@ -2,17 +2,23 @@ let initialState = {
 	counters: []
 };
 
-// This reducer isn't pure! It updates the nested "counters" array of the original 
+// This reducer isn't pure! It updates the nested "counters" array of the original
 //     state object
 const reducer = (state = initialState, action) => {
 	let { type, counterIndex } = action;
+
+	let stateCopy = {
+		...state,
+		counters: [...state.counters]
+	};
+
 	if (type == "INCREMENT") {
-		state.counters[counterIndex]++;
-		return state;
+		stateCopy.counters[counterIndex]++;
+		return stateCopy;
 	} else if (type == "ADD_COUNTER") {
-		state.counters.push(0);
-		return state;
+		stateCopy.counters.push(0);
+		return stateCopy;
 	} else {
-		return state;
+		return stateCopy;
 	}
 }
